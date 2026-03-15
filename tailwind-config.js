4a580f88a027f30908b90163018cad743ab3610b
aadri05 <adrigueziban@gmail.com>
2026-03-15 19:22:32 +0100

    Creation of the page

---
+++ tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brandBlack: '#000000',
+                brandWhite: '#FFFFFF',
+                brandGray: '#F5F5F5',
+                brandSlate: '#1A1A1A',
+            },
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+            },
+            letterSpacing: {
+                widest: '.25em',
+            }
+        }
+    }
+}
